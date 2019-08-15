64b298bee64b65cb25b1c1deb57c0c4d88db9de1
Minh Tùng <leminhtung1111998@gmail.com>
2019-08-15 09:24:55 +0700

    New

--- WebApplication1/Models/User.cs
+++ WebApplication1/Models/User.cs
@@ -7,16 +7,17 @@ namespace WebApplication1.Models
     {
         public int UserId { get; set; }
 
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "username is too long")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "characters between 3 and 50")]
         [Required(ErrorMessage = "username can not be blank")]
         public string Username { get; set; }
 
         [StringLength(50)]
         [Required(ErrorMessage = "PhoneNumber can not be blank")]
-        [RegularExpression(@"^[0-9]{9,10}$", ErrorMessage = "Phone number wrong format")]
+        [RegularExpression(@"^(09|08[3|6|8|9])+([0-9]{7})$", ErrorMessage = "Phone number wrong format")]
         public string PhoneNumber { get; set; }
 
         [StringLength(50)]
+        //[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}
         [DataType(DataType.EmailAddress)]
         [Required(ErrorMessage = "Email can not be blank")]
         public string Email { get; set; }
